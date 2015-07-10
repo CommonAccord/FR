dc85d75d873ffe9489d97c4e302dbf581f2068e3
James Hazard <james.g.hazard@gmail.com>
2015-07-10 16:20:58 +0200

    intro par jjveron
    http://github.com/jjveron

--- vendor/CommonAccord/cmacc/library/landing.php
+++ vendor/CommonAccord/cmacc/library/landing.php
@@ -12,14 +12,24 @@ include("header.php");
      <p align="center"><img src="visual/cmacc-trans.png" style="width:25%" /></p>
 <br/>
 <hr>
-<table><tr><td width="40%">
-<jjv></jjv>
+<table><tr><td width="50%">
+<h4>
+La première phase de l’informatisation juridique a consisté à stocker les textes sous une forme numérique, permettant simplement de les consulter.
+<br><br>
+Une deuxième phase a entrepris de coder des contenus spécialisés (fiscalité, finances, médecine) pour les  soumettre à des règles et ouvrir la voie à des prises de décision automatisées.
+<br><br>
+Nous entrons depuis peu dans une troisième phase, celle des contrats intelligents « auto exécutoires » : la technologie ne sera plus seulement une aide à la prise de décision, mais également un moyen de faire respecter les règles. Ainsi, les systèmes de Gestion des Droits Numériques intègrent-t-ils les règles légales du droit d’auteur dans des dispositifs techniques de protection. Les contrats intelligents éliminent toute ambiguïté d’interprétation, ils ne nécessitent plus l’existence d’un lien de confiance entre les parties, car elles sont assurées d’une exécution conforme à la convention.
+<br><br>
+  C’est ainsi que CommonAccord automatise la rédaction de documents juridiques, en créant un modèle global de textes juridiques codifiés semblable aux règles Incoterms pour les transactions commerciales. Les documents sont à ce point modulaires qu’une grande partie du texte n’est plus affiché, laissant aux parties le soin de ne traiter que les seuls points spécifiques, ce qui clarifie les relations. Accessibles en « open source », les textes peuvent être améliorés, prolongés et utilisés par la communauté. Ce système permet d’envisager la création dynamique de contrats intelligents sophistiqués et hautement personnalisés, automatiquement restituables dans un document juridique exécutoire entre les parties. <a href="https://github.com/jjveron">(&nbsp;Introduction par jjveron&nbsp;)</a>
+<br><br>
+Exemple (Cliquer ensuite sur Source et Edit pour visualiser les détails) : <a href="http://www.commonaccord.org/index.php?action=doc&file=/FR/AdC/CNRS_AdC_Demo.md">Accord de confidentialité</a>
+</h4>
 <h2>vers un <a href="http://bitly.com/1DWGTxs">Code Civil 3.0</a></h2><br><br>
         <h2 ><a href=<?=$_SERVER['PHP_SELF']?>?action=list&file=/>Documents</a></h2>
 
 </td><td> &emsp; 
 </td><td> &emsp; 
-</td><td width="40%">
+</td><td width="30%">
 
 <a class="twitter-timeline" href="https://twitter.com/CommonAccord/with_replies" data-widget-id="574817616360964096" width="600" height="300">Tweets by @CommonAccord</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
